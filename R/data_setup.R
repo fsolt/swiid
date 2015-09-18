078fa08ffaf2f91be0833742535cb08213c9fad3
Frederick Solt <fsolt@mac.com>
2015-09-18 07:16:29 -0500

    import sedlac pc

--- R/data_setup.R
+++ R/data_setup.R
@@ -4,4 +4,4 @@ library(readxl)
 library(reshape2)
 
 download.file("http://sedlac.econo.unlp.edu.ar/download.php?file=archivos_estadistica/inequality_LAC_2015-06.xls", "data-raw/sedlac.xls")
-sedlac_pc <- read_excel("data-raw/sedlac.xls", sheet = 3, skip = 8)
+sedlac_pc <- read_excel("data-raw/sedlac.xls", sheet = "intervals pci", skip = 8)
