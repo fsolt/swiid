4e1d01ef7e07b47a11e90c74a837ccf316ffd401
Frederick Solt <fsolt@mac.com>
2015-09-17 20:59:57 -0500

    initial commit

---
+++ R/data_setup.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(readr)
+library(readxl)
+library(reshape2)
+
+download.file("http://sedlac.econo.unlp.edu.ar/download.php?file=archivos_estadistica/inequality_LAC_2015-06.xls", "data-raw/sedlac.xls")
+sedlac_pc <- read_excel("data-raw/sedlac.xls", sheet = 3, skip = 8)
