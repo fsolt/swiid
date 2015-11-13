52517e1c562daafc6d393cc291e492780a0ec761
Frederick Solt <fsolt@mac.com>
2015-11-13 16:10:51 -0600

    add tidyr

--- R/data_setup.R
+++ R/data_setup.R
@@ -2,6 +2,16 @@ library(dplyr)
 library(readr)
 library(readxl)
 library(reshape2)
+library(eurostat)
+library(tidyr)
 
+###NEITHER OF THESE IS DONE
+
+# Sedlac
 download.file("http://sedlac.econo.unlp.edu.ar/download.php?file=archivos_estadistica/inequality_LAC_2015-06.xls", "data-raw/sedlac.xls")
 sedlac_pc <- read_excel("data-raw/sedlac.xls", sheet = "intervals pci", skip = 8)
+
+
+
+# Eurostat (no flags for series breaks)
+eurostat <- get_eurostat("ilc_di12", time_format = "num") %>% label_eurostat()
