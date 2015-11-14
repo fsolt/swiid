d3fb84a57132483e0924d8d74282087c4b9fd600
Frederick Solt <fsolt@mac.com>
2015-11-14 09:24:38 -0600

    work on sedlac

--- R/data_setup.R
+++ R/data_setup.R
@@ -10,7 +10,11 @@ library(tidyr)
 # Sedlac
 download.file("http://sedlac.econo.unlp.edu.ar/download.php?file=archivos_estadistica/inequality_LAC_2015-06.xls", "data-raw/sedlac.xls")
 sedlac_pc <- read_excel("data-raw/sedlac.xls", sheet = "intervals pci", skip = 8)
-
+countries_sedlac <- c("Argentina", "Bolivia", "Brazil", "Chile", "Colombia",
+                      "Costa Rica", "Dominican Rep.", "Ecuador", "El Salvador",
+                      "Guatemala", "Honduras", "Mexico", "Nicaragua", "Panama",
+                      "Paraguay", "Peru", "Uruguay", "Venezuela", "The Caribbean",
+                      "Guyana", "Haiti", "Jamaica", "Suriname")
 
 
 # Eurostat (no flags for series breaks)
