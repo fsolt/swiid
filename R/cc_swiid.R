6f2eb671156670bac6347c98871f5ae1abb28d19
Frederick Solt <fsolt@mac.com>
2017-06-23 21:28:56 -0500

    fix regex

--- R/cc_swiid.R
+++ R/cc_swiid.R
@@ -8,10 +8,10 @@ cc_swiid <- countrycode::countrycode_data %>%
            str_replace("Democratic Republic of the Congo", "Congo, D.R.") %>% 
            str_replace("Lao People's Democratic Republic", "Lao"),
          country.name.en.regex = if_else(swiid.name == "Russian Federation",
-                                          "\\brussia",
-                                          if_else(swiid.name == "Central African Republic",
-                                                  "\\bcentral.african.rep",
-                                                  country.name.en.regex))) %>% 
+                                         "\\brussia",
+                                         if_else(swiid.name == "Central African Republic",
+                                                 "\\bcentral.african.rep",
+                                                 country.name.en.regex))) %>% 
   filter(!country.name.en == "Korea") %>% 
   full_join(tibble(swiid.name = "Soviet Union", 
                    country.name.en.regex = "soviet.?union|u\\.?s\\.?s\\.?r|socialist.?republics"),
