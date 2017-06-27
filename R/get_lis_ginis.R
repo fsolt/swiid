3e2b55dbadfa99868144646a587c358cc49dba72
Frederick Solt <fsolt@mac.com>
2017-06-27 13:00:19 -0500

    get by country--not by wd_es--and switch to lissy.R; add delay to avoid bogging down lissy

--- R/get_lis_ginis.R
+++ R/get_lis_ginis.R
@@ -2,38 +2,39 @@ library(tidyverse)
 library(stringr)
 library(mailR)
 
-weldef_eqsc <- c("market_hh", "market_sqrt", "market_pc",
-                 "disp_hh", "disp_sqrt", "disp_pc",
-                 "con_hh", "con_sqrt", "con_pc")
+ccs <- c("au", "at", "be", "br", "ca", "cl", "cn", "co", "cz", "dk", 
+         "do", "eg", "ee", "fi", "fr", "de", "ge", "gr", "gt", "hu", "is", 
+         "in", "ie", "il", "it", "jp", "lu", "mx", "nl", "no", "pa", "py", 
+         "pe", "pl", "ro", "ru", "rs", "sk", "si", "za", "kr", "es", "se", 
+         "ch", "tw", "uk", "us", "uy")
 
-get_lis_ginis <- function(wd_es) {
+get_lis_ginis <- function(ccs, wait = 900) {
   gmail_user <- getOption("gmail_user")         # set all of these in .Rprofile, e.g.: 
   gmail_password <- getOption("gmail_password") # options("gmail_user" = "juanita_herrara@gmail.com", 
   lis_user <- getOption("lis_user")             #         "gmail_password" = "my_password")
   lis_password <- getOption("lis_password")
   
-  purrr::walk(wd_es, function(weldef_eqsc) {
-    wt <- ifelse(str_detect(weldef_eqsc, "hh"), "hpopwgt", "hpopwgt*nhhmem")
-    
-    lissy <- readLines("R/lissy_stata.txt") %>% 
+  purrr::walk(ccs, function(cc) {
+    lissy <- readLines("R/lissy.R") %>% 
       paste(collapse = "\n") %>% 
       str_replace("lis_user", lis_user) %>% 
       str_replace("lis_password", lis_password) %>% 
-      str_replace_all("WD_ES", weldef_eqsc) %>% 
-      str_replace("WT", wt)
+      str_replace_all("CCODE", cc)
 
     mailR::send.mail(from = gmail_user,
                      to = "postbox@lisdatacenter.org",
-                     subject = weldef_eqsc,
+                     subject = cc,
                      body = lissy,
                      smtp = list(host.name = "smtp.gmail.com",
                                  port = 465, 
                                  user.name = gmail_user, 
                                  passwd = gmail_password,
-                                 ssl=TRUE),
+                                 ssl = TRUE),
                      authenticate = TRUE,
                      send = TRUE)
+    
+    if (length(ccs) > 1) Sys.sleep(wait)
   })
 } 
 
-get_lis_ginis(weldef_eqsc)
+get_lis_ginis(ccs)
