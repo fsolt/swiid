da30a67e7ce7611cb00b6968df05e9d94a441f54
Frederick Solt <fsolt@mac.com>
2017-07-15 11:25:41 -0500

    use kt as index for gini (with country-specific t)

--- R/lis_plus_series.R
+++ R/lis_plus_series.R
@@ -11,24 +11,49 @@ chains <- 4
 cores <- chains
 gt <- 3
 
-x <- ineq %>%  
+x0 <- ineq %>%  
   filter(k_bl_obs > gt & s_bl_obs > 1) %>% 
-  mutate(kcode = as.integer(factor(country, levels = unique(country))), # redo codes for filtered sample
-         tcode = as.integer(year - min(year) + 1),
-         scode = as.integer(factor(series, levels = unique(series))))
+  mutate(kcode = as.integer(factor(country, levels = unique(country)))) %>%  # redo codes for filtered sample
+  group_by(country) %>% 
+  mutate(tcode = as.integer(year - min(year) + 1)) %>% 
+  ungroup()
+
+kt <- x0 %>%     
+  group_by(kcode) %>%
+  summarize(firstyr = min(year),
+            lastyr = max(year),
+            yrspan = (lastyr - firstyr) + 1) %>% 
+  ungroup() %>%  
+  slice(rep(1:n(), yrspan)) %>% 
+  group_by(kcode) %>% 
+  mutate(tcode = 1:n()) %>% 
+  ungroup() %>% 
+  mutate(ktcode = 1:n())
+
+x <- x0 %>% 
+  left_join(kt, by = c("kcode", "tcode"))
+
+kn <- x %>% 
+  group_by(kcode) %>% 
+  summarize(kt1 = first(ktcode),
+            yrspan = first(yrspan)) %>% 
+  ungroup()
 
 # Format data for Stan
 source_data <- list(  K = max(x$kcode),
                       T = max(x$tcode),
+                      KT = nrow(kt),
                       S = max(x$scode),
-                      N = length(x$gini_m),
+                      N = length(x$gini_b),
                       N_b = length(x$gini_b[!is.na(x$gini_b)]),
                       kk = x$kcode,
                       tt = x$tcode,
-                      kktt = (x$kcode-1)*max(x$tcode)+x$tcode,
+                      kktt = x$ktcode,
+                      ktt = kt$tcode,
+                      ktk = kt$kcode,
+                      kn = kn$yrspan,
+                      kt1 = kn$kt1,
                       ss = x$scode,
-                      ktt = rep(1:max(x$tcode), times = max(x$kcode)),
-                      ktk = rep(1:max(x$kcode), each = max(x$tcode)),
                       gini_m = x$gini_m,
                       gini_m_se = x$gini_m_se,
                       gini_b = x$gini_b[!is.na(x$gini_b)],
@@ -58,101 +83,6 @@ beep() # chime
 
 
 # Post-processing
-plot_tscs <- function(input, output, kt = FALSE, pars="gini", probs=c(.025, .975),
-                      dims, year_bounds, y_label, save_pdf = NA) {
-  
-  kcodes <- input %>%
-    group_by(country) %>%
-    summarize(kcode = first(kcode),
-              firstyr = min(year),
-              lastyr = max(year)) %>%
-    ungroup()
-  
-  ktcodes <- tibble(kcode = rep(1:max(x$kcode), each = max(x$tcode)),
-                    tcode = rep(1:max(x$tcode), times = max(x$kcode)),
-                    ktcode = (kcode-1)*max(tcode)+tcode) %>%
-    left_join(kcodes, by = "kcode") %>%
-    mutate(year = min(firstyr) + tcode - 1)  
-  
-  lb <- paste0("x", str_replace(probs*100, "\\.", "_"), "percent")[1]
-  ub <- paste0("x", str_replace(probs*100, "\\.", "_"), "percent")[2]
-  
-  if (!kt) {
-    gini_res <- rstan::summary(output, pars=pars, probs=probs) %>%
-      first() %>%
-      as.data.frame() %>%
-      rownames_to_column("parameter") %>%
-      as_tibble() %>%
-      janitor::clean_names() %>% 
-      mutate(estimate = mean,
-             lb = get(paste0("x", str_replace(probs*100, "\\.", "_"), "percent")[1]),
-             ub = get(paste0("x", str_replace(probs*100, "\\.", "_"), "percent")[2]),
-             kcode = as.numeric(str_extract(parameter, "(?<=\\[)\\d+")),
-             tcode = as.numeric(str_extract(parameter, "(?<=,)\\d+"))) %>%
-      left_join(ktcodes, by=c("kcode", "tcode")) %>%
-      arrange(kcode, tcode)
-  } else {
-    gini_res <- rstan::summary(output, pars=pars, probs=probs) %>%
-      first() %>%
-      as.data.frame() %>%
-      rownames_to_column("parameter") %>%
-      as_tibble() %>%
-      janitor::clean_names() %>% 
-      mutate(estimate = mean,
-             lb = get(paste0("x", str_replace(probs*100, "\\.", "_"), "percent")[1]),
-             ub = get(paste0("x", str_replace(probs*100, "\\.", "_"), "percent")[2]),
-             ktcode = as.numeric(str_extract(parameter, "(?<=\\[)\\d+"))) %>%
-      left_join(ktcodes, by="ktcode")
-    arrange(kcode, tcode)
-  }
-  
-  if (missing(dims)) {
-    if (!missing(save_pdf)) {
-      dims <- c(7, 5)
-    } else {
-      dims <- c(ceiling(max(input$kcode)/5), 5)
-    }
-  }
-  
-  rows <- dims[1]
-  cols <- dims[2]
-  
-  if (missing(year_bounds)) {
-    year_bounds <- c(1960, ceiling(as.integer(format(Sys.Date(), "%Y"))/10)*10)
-  }
-  
-  if (missing(y_label)) y_label <- "Gini Coefficient"
-  
-  npages <- ceiling(max(gini_res$kcode)/(rows*cols))
-  pages <- paste(((0:(npages - 1) * rows * cols) + 1), (1:npages * rows * cols), sep = ":")
-  
-  for (i in 1:npages) {
-    cpage <- unique(gini_res$country)[((i-1)*rows*cols)+1:i*rows*cols]
-    cpage <- unique(gini_res$country)[c(eval(parse(text=pages[i])))]
-    cp <- gini_res[gini_res$country %in% cpage, ]
-    cp$country <- factor(cp$country, levels = cpage)
-    
-    plotx <- ggplot(data=cp, aes(x=year, y=estimate)) +
-      geom_line() + theme_bw() +
-      theme(legend.position="none") +
-      coord_cartesian(xlim=year_bounds) +
-      labs(x = NULL, y = y_label) +
-      geom_ribbon(aes(ymin = lb, ymax = ub, linetype=NA), alpha = .25) +
-      facet_wrap(~country, ncol = cols) +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1),
-            strip.background = element_rect(fill = "white", colour = "white"))
-    
-    if (i==1) plotx1 <- plotx
-    
-    if (!is.na(save_pdf)) {
-      pdf(file=str_replace(save_pdf, "\\.", paste0(i, ".")), width=6, height = 9)
-      plot(plotx)
-      graphics.off()
-    }
-  }
-  
-  return(plotx1)
-}
-
+source("R/plot_tscs.R")
 plot_tscs(x, out1, save_pdf = "paper/figures/ts_lps.pdf")
 plot_tscs(x, out1)
--- R/lis_plus_series.stan
+++ R/lis_plus_series.stan
@@ -1,11 +1,17 @@
 data {
   int<lower=1> K;     		                // number of countries
-  int<lower=1> T; 				                // number of years
+  int<lower=1> T;     		                // (maximum) number of years
+  int<lower=1> KT;                        // number of observed & interpolated country-years
   int<lower=1> S; 				                // number of series
   int<lower=0> N;                         // total number of observations
   int<lower=0> N_b;                       // total number of observations with baseline
   int<lower=1, upper=K> kk[N]; 	          // country for observation n
   int<lower=1, upper=T> tt[N]; 	          // year for observation n
+  int<lower=1, upper=KT> kktt[N];         // country-year for observation n
+  int<lower=1, upper=T> ktt[KT]; 	        // year for country-year kt ("country-year-year")
+  int<lower=1, upper=K> ktk[KT]; 	        // country for country-year kt ("country-year-country")
+  int<lower=1, upper=T> kn[K];            // number of observed & interpolated country-years by country
+  int<lower=1, upper=KT> kt1[K];          // location of first kt for country k
   int<lower=1, upper=S> ss[N];            // series for observation n
   vector<lower=0, upper=1>[N] gini_m; 	  // measured gini for observation n
   vector<lower=0, upper=1>[N] gini_m_se;  // std error of measured gini for obs n
@@ -14,8 +20,8 @@ data {
 }  
   
 parameters {
-  row_vector<lower=0, upper=1>[T] gini[K];  // SWIID gini estimate of baseline in country k at time t
-  real<lower=0, upper=.02> sigma_gini[K]; 	// country variance parameter (see Linzer and Stanton 2012, 12)
+  real<lower=0, upper=1> gini[KT];        // SWIID gini estimate for baseline in country k at time t
+  real<lower=0> sigma_gini[K];            // country variance parameter (see Linzer and Stanton 2012, 12)
   vector<lower=0, upper=1>[N] gini_t;   
   
   vector<lower=0>[S] rho_s;            
@@ -23,21 +29,25 @@ parameters {
 }
 
 model {
+  sigma_gini ~ normal(0, .015);
   gini_t ~ normal(gini_m, gini_m_se);
   rho_s ~ normal(1, .25);
 
   for (k in 1:K) {
-    gini[k][1] ~ normal(.35, .1);                         // a random draw from N(.35, .1) in first year
-    gini[k][2:T] ~ normal(gini[k][1:T-1], sigma_gini[k]); // otherwise a random walk from previous year 
+    if (kn[k] > 1) {
+      gini[kt1[k]] ~ normal(.35, .1);                         // a random draw from N(.35, .1) in first year
+      gini[(kt1[k]+1):(kt1[k]+kn[k]-1)] ~ normal(gini[(kt1[k]):(kt1[k]+kn[k]-2)], sigma_gini[k]); // otherwise a random walk from previous year 
+    } else {
+      gini[kt1[k]] ~ normal(.35, .1);                            // a random draw from N(.35, .1)
+    }
   }
   
   for (n in 1:N) {
     if (n <= N_b) {
-      gini[kk[n]][tt[n]] ~ normal(gini_b[n], gini_b_se[n]); // use baseline series where observed
+      gini[kktt[n]] ~ normal(gini_b[n], gini_b_se[n]); // use baseline series where observed
       gini_b[n] ~ normal(rho_s[ss[n]] * gini_t[n], sigma_s); // estimate rho_s 
     } else {
-      gini[kk[n]][tt[n]] ~ normal(gini_t[n] * rho_s[ss[n]], sigma_s); // estimate gini
+      gini[kktt[n]] ~ normal(gini_t[n] * rho_s[ss[n]], sigma_s); // estimate gini
     }
   }
-
 }
