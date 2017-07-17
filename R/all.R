ab27df0af2cedf37d3e9699345bdd671a4d5e2f8
Frederick Solt <fsolt@mac.com>
2017-07-17 15:56:16 -0500

    revert, but with fixed constraints

--- R/all.R
+++ R/all.R
@@ -6,7 +6,7 @@ library(beepr)
 load("data/ineq.rda")
 
 seed <- 324
-iter <- 10
+iter <- 20
 chains <- 1
 cores <- chains
 
@@ -41,11 +41,20 @@ source_data <- list(  K = max(x$kcode),
                       WE = max(x$wecode),
                       KWE = max(x$kwecode),
                       RWE = max(x$rwecode),
-
+                      W = max(x$wcode),
+                      KW = max(rho_wd$kwcode),
+                      RW = max(rho_wd$rwcode),
+                      E = max(x$ecode),
+                      KE = max(rho_es$kecode),
+                      RE = max(rho_es$recode),
+                      
                       N = nrow(x),
                       N_bl = nrow(x %>% filter(bl)),
                       N_obl = nrow(x %>% filter(obl)),
                       N_kbl = nrow(x %>% filter(kbl)),
+                      N_kk = nrow(x %>% filter(kbl | (kw & ke))),
+                      N_kr = nrow(x %>% filter(kbl | kw)),
+                      N_rk = nrow(x %>% filter(kbl | kw | ke)),
                       
                       kk = x$kcode,
                       tt = x$tcode,
@@ -61,6 +70,10 @@ source_data <- list(  K = max(x$kcode),
                       wen = x$wecode,
                       kwen = x$kwecode,
                       rwen = x$rwecode,
+                      kwn = x$kwcode,
+                      ken = x$kecode,
+                      rwn = x$rwcode,
+                      ren = x$recode,
                       
                       gini_m = x$gini_m,
                       gini_m_se = x$gini_m_se,
@@ -75,7 +88,27 @@ source_data <- list(  K = max(x$kcode),
                       kwem = rho_we$kwecode,
                       rwem = rho_we$rwecode,
                       rho_we = rho_we$rho,
-                      rho_we_se = rho_we$rho_se
+                      rho_we_se = rho_we$rho_se,
+                      
+                      P = length(rho_wd1$rho_wd),
+                      kkp = rho_wd1$kcode,      
+                      rrp = rho_wd1$rcode,
+                      ttp	= rho_wd1$tcode,
+                      wdp = rho_wd1$wcode,
+                      kwp = rho_wd1$kwcode,
+                      rwp = rho_wd1$rwcode,
+                      rho_wd = rho_wd1$rho_wd,
+                      rho_wd_se = rho_wd1$rho_wd_se,
+                      
+                      Q = length(rho_es1$rho_es),
+                      kkq = rho_es1$kcode,      
+                      rrq = rho_es1$rcode,
+                      ttq	= rho_es1$tcode,
+                      esq = rho_es1$ecode,
+                      keq = rho_es1$kecode,
+                      req = rho_es1$recode,
+                      rho_es = rho_es1$rho_es,
+                      rho_es_se = rho_es1$rho_es_se
 )
 
 # Stan
