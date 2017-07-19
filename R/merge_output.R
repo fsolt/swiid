bb5f9609642ca4e938d4ac6098da98e341018fe9
Frederick Solt <fsolt@mac.com>
2017-07-19 13:28:58 -0500

    add merge_output

---
+++ R/merge_output.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+
+summary_kt <- function(input, output, probs = c(.025, .975)) {
+  ktcodes <- input %>%  
+    transmute(kcode = kcode,
+              country = country,
+              firstyr = firstyr,
+              yrspan = (lastyr - firstyr) + 1) %>% 
+    distinct() %>% 
+    slice(rep(1:n(), yrspan)) %>% 
+    group_by(kcode) %>% 
+    mutate(tcode = 1:n(),
+           year = firstyr + tcode - 1) %>% 
+    ungroup() %>% 
+    mutate(ktcode = 1:n())
+
+  gini_res <- rstan::summary(output, pars="gini", probs=probs) %>%
+    first() %>%
+    as.data.frame() %>%
+    rownames_to_column("parameter") %>%
+    as_tibble() %>%
+    janitor::clean_names() %>% 
+    mutate(gini = mean,
+           lb = get(paste0("x", str_replace(probs*100, "\\.", "_"), "percent")[1]),
+           ub = get(paste0("x", str_replace(probs*100, "\\.", "_"), "percent")[2]),
+           se = round((ub - lb)/4, 4),
+           ktcode = as.numeric(str_extract(parameter, "(?<=\\[)\\d+"))) %>%
+    left_join(ktcodes, by="ktcode") %>% 
+    select(country, year, gini, lb, ub, se, ktcode) %>% 
+    arrange(ktcode)
+
+  return(gini_res)
+}
+
+
+not_lis_file <- list.files("data/", "all_not_lis") %>% 
+  last() %>% 
+  file.path("data", .)
+lis_file <- list.files("data/", "all_lis") %>% 
+  last() %>% 
+  file.path("data", .)
+
+load(not_lis_file)
+not_lis_in <- x
+not_lis_out <- out1
+
+load(lis_file)
+lis_in <- x
+lis_out <- out1
+
+
+
