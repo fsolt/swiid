62290a823f21bec63ddeb4ec8c8f79bba878a5ca
Frederick Solt <fsolt@mac.com>
2018-04-25 13:06:22 -0500

    update cc_swiid for v1.0 countrycode

--- R/cc_swiid.R
+++ R/cc_swiid.R
@@ -1,18 +1,23 @@
 regions <- read_csv("data/reg.csv", col_types = "cc") %>%
-  mutate(swiid.name = if_else(swiid.name=="C̫te D'Ivoire", "Côte D'Ivoire", swiid.name))
+  mutate(swiid.name = if_else(str_detect(swiid.name, "Ivoire"), "Côte d'Ivoire", swiid.name),
+         swiid.name = if_else(str_detect(swiid.name, "ncipe"), "São Tomé and Príncipe", swiid.name))
 write_csv(regions, "data/reg.csv")
 
 cc_swiid <- countrycode::codelist %>% 
+  select(country.name.en, country.name.en.regex, iso2c, iso3c, wb_api3c) %>% 
   mutate(swiid.name = country.name.en %>% 
+           str_replace(" & ", " and ") %>% 
            str_replace(" \\(.*", "") %>% 
            str_replace(", [^U]*", "") %>% 
            str_replace("^(United )?Republic of ", "") %>% 
            str_replace("^The former Yugoslav Republic of ", "") %>% 
            str_replace(" of [GA].*", "") %>% 
-           str_replace("Democratic Republic of the Congo", "Congo, D.R.") %>% 
-           str_replace("Lao People's Democratic Republic", "Lao") %>% 
+           str_replace("Czechia", "Czech Republic") %>% 
            str_replace("Russian Federation", "Russia") %>% 
-           str_replace("Syrian Arab Republic", "Syria"),
+           str_replace("Syrian Arab Republic", "Syria") %>% 
+           str_replace("Côte d’Ivoire", "Côte d'Ivoire") %>% 
+           str_replace("Hong Kong SAR China", "Hong Kong") %>% 
+           str_replace("South Korea", "Korea"),
          country.name.en.regex = case_when(swiid.name == "Russia" ~ "\\brussia",
                                            swiid.name == "Central African Republic" ~ "\\bcentral.african.rep",
                                            swiid.name == "Micronesia" ~ "micronesia",
@@ -20,7 +25,6 @@ cc_swiid <- countrycode::codelist %>%
                                            swiid.name == "Dominica" ~ "dominica\\b",
                                            swiid.name == "Dominican Republic" ~ "dominican",
                                            TRUE ~ country.name.en.regex)) %>% 
-  filter(!country.name.en == "Korea") %>% 
   full_join(tibble(swiid.name = "Soviet Union", 
                    country.name.en.regex = "soviet.?union|u\\.?s\\.?s\\.?r|socialist.?republics"),
             by = c("country.name.en.regex", "swiid.name")) %>% 
