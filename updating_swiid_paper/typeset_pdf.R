757912258c433ce997cf44567b983040686694b5
Frederick Solt <fsolt@mac.com>
2018-05-08 09:54:53 -0500

    rely on rmarkdown::render() only

--- updating_swiid_paper/typeset_pdf.R
+++ updating_swiid_paper/typeset_pdf.R
@@ -3,9 +3,9 @@ typeset_pdf <- function(path_to_Rmd) {
   file_name_stub <- stringr::str_replace(path_to_Rmd, ".*/(.*).Rmd", "\\1")
   path_to_tex <- paste0(tools::file_path_sans_ext(path_to_Rmd), ".tex")
   path_to_pdf <- paste0(tools::file_path_sans_ext(path_to_Rmd), ".pdf")
-  rmarkdown::render(path_to_Rmd)
-  tools::texi2pdf(file = path_to_tex, clean = TRUE)
-  file.rename(paste0(file_name_stub, ".pdf"), path_to_pdf)
+  try(rmarkdown::render(path_to_Rmd))
+  # tools::texi2pdf(file = path_to_tex, clean = TRUE)
+  # file.rename(paste0(file_name_stub, ".pdf"), path_to_pdf)
   system(paste0("osascript -e", " 'tell application", ' "Preview" to close (every window whose name contains "', file_name_stub, '")', "'"))
   system(paste0('open "', path_to_pdf, '"'))
 }
