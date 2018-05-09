9f8c27ddb7e5fb27c937236a0ed7eaf39880c766
Frederick Solt <fsolt@mac.com>
2018-05-09 17:36:54 -0500

    move into dir before rendering; clean up

--- updating_swiid_paper/typeset_pdf.R
+++ updating_swiid_paper/typeset_pdf.R
@@ -1,11 +1,12 @@
 typeset_pdf <- function(path_to_Rmd) {
-  folder_with_Rmd <- stringr::str_replace(path_to_Rmd, "(.*)/.*.Rmd", "\\1")
-  file_name_stub <- stringr::str_replace(path_to_Rmd, ".*/(.*).Rmd", "\\1")
-  path_to_tex <- paste0(tools::file_path_sans_ext(path_to_Rmd), ".tex")
-  path_to_pdf <- paste0(tools::file_path_sans_ext(path_to_Rmd), ".pdf")
-  try(rmarkdown::render(path_to_Rmd))
-  # tools::texi2pdf(file = path_to_tex, clean = TRUE)
-  # file.rename(paste0(file_name_stub, ".pdf"), path_to_pdf)
+  dir_with_Rmd <- stringr::str_replace(path_to_Rmd, "(.*/).*.Rmd", "\\1")
+  orig_path <- getwd()
+  setwd(dir_with_Rmd)
+  file_name <- stringr::str_replace(path_to_Rmd, dir_with_Rmd, "")
+  try(rmarkdown::render(file_name))
   system(paste0("osascript -e", " 'tell application", ' "Preview" to close (every window whose name contains "', file_name_stub, '")', "'"))
-  system(paste0('open "', path_to_pdf, '"'))
+  system(paste0('open "', paste0(stringr::str_replace(file_name, ".Rmd", ".pdf")), '"'))
+  setwd(orig_path)
 }
+
+# typeset_pdf("updating_swiid_paper/updating_swiid.Rmd")
