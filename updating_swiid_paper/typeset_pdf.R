31494403ab23583894252d666c6e49854c62b6e9
Frederick Solt <fsolt@mac.com>
2018-06-03 06:59:15 -0500

    switch to tools::texi2pdf on error

--- updating_swiid_paper/typeset_pdf.R
+++ updating_swiid_paper/typeset_pdf.R
@@ -3,8 +3,9 @@ typeset_pdf <- function(path_to_Rmd) {
   orig_path <- getwd()
   setwd(dir_with_Rmd)
   file_name <- stringr::str_replace(path_to_Rmd, dir_with_Rmd, "")
-  try(rmarkdown::render(file_name))
-  system(paste0("osascript -e", " 'tell application", ' "Preview" to close (every window whose name contains "', file_name_stub, '")', "'"))
+  tryCatch(rmarkdown::render(here::here(dir_with_Rmd, file_name)),
+      error = function(e) tools::texi2pdf(file = stringr::str_replace(file_name, ".Rmd", ".tex"), clean = TRUE))
+  system(paste0("osascript -e", " 'tell application", ' "Preview" to close (every window whose name contains "', stringr::str_replace(file_name, ".Rmd", ".pdf"), '")', "'"))
   system(paste0('open "', paste0(stringr::str_replace(file_name, ".Rmd", ".pdf")), '"'))
   setwd(orig_path)
 }
