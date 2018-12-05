f775f7ba84b83f48805aa5d60773c94b21e8c47b
Frederick Solt <frederick-solt@uiowa.edu>
2018-12-05 12:46:43 -0600

    move prob_perc to kfold_output

--- R/estimate_swiid/kfold/combine_folds_hpc.R
+++ R/estimate_swiid/kfold/combine_folds_hpc.R
@@ -5,26 +5,30 @@ output_path <- "/Volumes/fsolt/swiid_kfold/output"
   str_extract("\\d+(?=\\.)") %>%
   unique())
 output_files <- list.files(output_path) %>% 
-  str_subset(output_file_ids %>% nth(5))
+  str_subset(output_file_ids %>% nth(length(output_file_ids)))
 
 kfold_output <- map_dfr(output_files, function(output_file) {
   if(readLines(file.path(output_path, output_file)) %>% 
      paste(collapse = "") %>% 
      str_detect("structure")) {
     (readLines(file.path(output_path, output_file)) %>% 
-    paste(collapse = "") %>% 
-    str_extract("structure.*L\\)\\)") %>% 
-    parse(text = .) %>% 
-    eval() %>% 
-    mutate(fold = str_extract(output_file, "\\d{1,2}$")))
-    }
-})
+       paste(collapse = "") %>% 
+       str_extract("structure.*L\\)\\)") %>% 
+       parse(text = .) %>% 
+       eval() %>% 
+       mutate(fold = str_extract(output_file, "\\d{1,2}$")))
+  }
+}) %>% 
+  group_by(country) %>% 
+  mutate(prob_perc = mean(problem, na.rm = TRUE),
+         t_diff = point_diff/se_diff) %>% 
+  ungroup()
+
 mean(kfold_output$problem, na.rm = TRUE)
 nrow(kfold_output)
 
 kfold_output %>% 
   group_by(country) %>%
-  mutate(prob_perc = mean(problem, na.rm = TRUE), t_diff = point_diff/se_diff) %>%
   select(country, year, fold, problem, prob_perc, 
          gini_b, gini_b_se, mean, sd,
          point_diff, se_diff, t_diff) %>%
