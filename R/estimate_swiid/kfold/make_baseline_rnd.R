5e49eacf144bde3664ae43fb39ca5048f1129796
Frederick Solt <frederick-solt@uiowa.edu>
2018-12-22 10:25:07 -0600

    fix ext for ineq0.rda

--- R/estimate_swiid/kfold/make_baseline_rnd.R
+++ R/estimate_swiid/kfold/make_baseline_rnd.R
@@ -1,4 +1,4 @@
-download.file("https://github.com/fsolt/swiid/raw/master/data/ineq0.Rda", "data/ineq0.rda")
+download.file("https://github.com/fsolt/swiid/raw/master/data/ineq0.rda", "data/ineq0.rda")
 source("https://raw.githubusercontent.com/fsolt/swiid/master/R/estimate_swiid/kfold/all_fold.R")
 baseline_rnd <- baseline_kfold(6)
 save(baseline_rnd, "data/baseline_rnd.rda")
