367fc0af179c7ab4617073c6af641d230777f3f9
Frederick Solt <frederick-solt@uiowa.edu>
2018-12-22 11:11:37 -0600

    use smaller folds; fix filepath for baseline_rnd.rda

--- R/estimate_swiid/kfold/make_baseline_rnd.R
+++ R/estimate_swiid/kfold/make_baseline_rnd.R
@@ -1,4 +1,4 @@
 download.file("https://github.com/fsolt/swiid/raw/master/data/ineq0.rda", "data/ineq0.rda")
 source("https://raw.githubusercontent.com/fsolt/swiid/master/R/estimate_swiid/kfold/all_fold.R")
-baseline_rnd <- baseline_kfold(6)
-save(baseline_rnd, "data/baseline_rnd.rda")
+baseline_rnd <- baseline_kfold(3)
+save(baseline_rnd, file = "R/estimate_swiid/kfold/baseline_rnd.rda")
