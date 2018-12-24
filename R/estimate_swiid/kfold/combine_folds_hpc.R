60485691eb5d672ba6feab739b667478c71405e7
Frederick Solt <frederick-solt@uiowa.edu>
2018-12-24 08:05:49 -0600

    update plot

--- R/estimate_swiid/kfold/combine_folds_hpc.R
+++ R/estimate_swiid/kfold/combine_folds_hpc.R
@@ -16,7 +16,7 @@ kfold_output <- map_dfr(output_files, function(output_file) {
        str_extract("structure.*L\\)\\)") %>% 
        parse(text = .) %>% 
        eval() %>% 
-       mutate(fold = str_extract(output_file, "\\d{1,2}$"),
+       mutate(fold = str_extract(output_file, "\\d{1,3}$"),
               cy = paste(country, year),
               cy_color = if_else(problem == 1, "#354995", "#5E5E5E"),
               point_diff = mean - gini_b %>% round(5)))
@@ -26,7 +26,8 @@ kfold_output <- map_dfr(output_files, function(output_file) {
   mutate(prob_perc = mean(problem, na.rm = TRUE),
          t_diff = point_diff/se_diff) %>% 
   ungroup() %>% 
-  arrange(point_diff)
+  arrange(point_diff) %>% 
+  add_count(country)
 mean(kfold_output$problem, na.rm = TRUE)
 nrow(kfold_output)
 
@@ -38,23 +39,28 @@ kfold_output %>%
   arrange(-problem, -prob_perc, country, -t_diff) %>%
   View()
 
+1 - mean(kfold_output %>% filter(n > 1) %>% pull(problem), na.rm = TRUE)
+mean(abs(kfold_output %>% filter(n > 1) %>% pull(point_diff)) < .01, na.rm = TRUE)
+mean(abs(kfold_output %>% filter(n > 1) %>% pull(point_diff)) < .02, na.rm = TRUE)
 
-ggplot(kfold_output) + 
-  # geom_rect(aes(xmin=-Inf, xmax=Inf, ymin=-2, ymax=2), fill='gray90', alpha=.2) +
+
+ggplot(kfold_output %>%
+         filter(n > 1)) +
   geom_hline(yintercept=0, linetype=2, colour="gray60") +
   geom_pointrange(fatten = 1,
                   aes(x = forcats::fct_reorder(cy, point_diff), 
                       y=point_diff*100, 
                       ymin=point_diff*100 - 1.96*100*se_diff,
                       ymax = point_diff*100 + 1.96*100*se_diff,
-                      colour = cy_color)) +
+                      colour = cy_color,
+                      alpha = problem)) +
   theme_bw() + 
   theme(legend.position="none") +
   scale_colour_manual(values=c("#354995", "#5E5E5E")) +
-  labs(x = "", y = "SWIID Prediction minus LIS") + 
-  theme(axis.text.x = element_blank(),
-        axis.ticks.x=element_blank()) +
-  # scale_y_continuous(breaks=c(-15, -10, -5, 0, 5)) + 
-  scale_x_discrete(limits=levels(kfold_output$cy)) 
+  scale_alpha_discrete(range = c(0.4, 1)) +
+  labs(x = "", y = latex2exp::TeX("SWIID \\textit{k}-fold Prediction minus LIS")) + 
+  theme(panel.grid.minor=element_blank()) +
+  scale_y_continuous(breaks=c(-10, -5, -2, -1,  0, 1, 2, 5, 10)) +
+  scale_x_discrete(breaks = NULL) 
 
-ggsave(file="../dcpo_redistribution/paper/warsaw_talk/kfold.pdf", width=8.5, height=5.25)
+ggsave(file="paper/figures/kfold_obs.pdf", width=8.5, height=5.25)
