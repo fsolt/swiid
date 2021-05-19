8ce6753ec99f37e761aff89001d8f528e413864f
Frederick Solt <frederick-solt@uiowa.edu>
2021-05-19 18:49:21 -0500

    add swiid_summary90 to comparisons

--- R/comparing_versions.R
+++ R/comparing_versions.R
@@ -2,6 +2,9 @@ library(tidyverse)
 
 swiid_summary_latest <- read_csv("data/swiid_summary.csv", col_types = "cddddddddd") %>% 
   mutate(decade = floor(year/10) * 10)
+swiid_summary90 <- "https://raw.githubusercontent.com/fsolt/swiid/eb799f41a08915ad4b6ab92c92f45b28995a93ed/data/swiid_summary.csv" %>% 
+  read_csv(col_types = "cddddddddd") %>% 
+  mutate(decade = floor(year/10) * 10)
 swiid_summary83 <- "https://raw.githubusercontent.com/fsolt/swiid/0a9b7cb0cee71497669318a9cb98f5cd5ae588ca/data/swiid_summary.csv" %>% 
   read_csv(col_types = "cddddddddd") %>% 
   mutate(decade = floor(year/10) * 10)
@@ -25,6 +28,10 @@ swiid_summary71 <- "https://github.com/fsolt/swiid/raw/master/data/swiid7_1_summ
 swiid_latest_83_71 <- swiid_summary_latest %>% 
   select(country, year, decade, gini_disp) %>% 
   rename(gini_latest = gini_disp) %>% 
+  left_join(swiid_summary90 %>%
+              select(country, year, decade, gini_disp) %>% 
+              rename(gini90 = gini_disp),
+            by = c("country", "year", "decade")) %>% 
   left_join(swiid_summary83 %>%
               select(country, year, decade, gini_disp) %>% 
               rename(gini83 = gini_disp),
@@ -33,7 +40,8 @@ swiid_latest_83_71 <- swiid_summary_latest %>%
               select(country, year, decade, gini_disp) %>% 
               rename(gini71 = gini_disp),
             by = c("country", "year", "decade")) %>% 
-  mutate(vlatest_minus83 = gini_latest - gini83,
+  mutate(vlatest_minus90 = gini_latest - gini90,
+         vlatest_minus83 = gini_latest - gini83,
          vlatest_minus71 = gini_latest - gini71,
          v83minus71 = gini83 - gini71)
 
@@ -57,6 +65,10 @@ swiid_latest_83_71_mkt <- swiid_summary_latest %>%
   mutate(decade = floor(year/10) * 10) %>% 
   select(country, year, decade, gini_mkt) %>% 
   rename(gini_latest = gini_mkt) %>% 
+  left_join(swiid_summary90 %>%
+              select(country, year, decade, gini_mkt) %>% 
+              rename(gini90 = gini_mkt),
+            by = c("country", "year", "decade")) %>% 
   left_join(swiid_summary83 %>%
               select(country, year, decade, gini_mkt) %>% 
               rename(gini83 = gini_mkt),
@@ -65,11 +77,12 @@ swiid_latest_83_71_mkt <- swiid_summary_latest %>%
               select(country, year, decade, gini_mkt) %>% 
               rename(gini71 = gini_mkt),
             by = c("country", "year", "decade")) %>% 
-  mutate(vlatest_minus83 = gini_latest-gini83,
+  mutate(vlatest_minus90 = gini_latest - gini90,
+         vlatest_minus83 = gini_latest-gini83,
          vlatest_minus71 = gini_latest-gini71,
          v83minus71 = gini83-gini71)
 
-# Mean within country-year differences by decade, Mark's advanced countries
+# Mean within country-year differences by decade
 swiid_latest_83_71_mkt %>%
   group_by(decade) %>%
   summarize_at(vars(starts_with("v")), mean, na.rm = TRUE) 
