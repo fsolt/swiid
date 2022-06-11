9b037b8e40fc10be83cabe83dafaeba4e29a499d
Frederick Solt <fsolt@mac.com>
2022-06-11 17:22:48 +0200

    use yahoo instead of gmail

--- R/get_lis_ginis.R
+++ R/get_lis_ginis.R
@@ -5,32 +5,30 @@ ccs <- c("au", "at", "be", "br", "ca", "ci", "cl", "cn", "co", "cz", "dk",
          "do", "eg", "ee", "fi", "fr", "de", "ge", "gr", "gt", "hu", "is", 
          "in", "ie", "il", "it", "jp", "lt", "lu", "mx", "nl", "no", "pa", "py", 
          "pe", "pl", "ps", "ro", "ru", "rs", "sk", "si", "za", "kr", "es", "se", 
-         "ch", "tw", "uk", "us", "uy", "vn")
+         "ch", "tw", "uk", "us", "uy", "vn") # la
 
-get_lis_ginis <- function(ccs, wait = 360) {
-  gmail_user <- getOption("gmail_user")         # set all of these in .Rprofile, e.g.: 
-  gmail_password <- getOption("gmail_password") # options("gmail_user" = "juanita_herrera@gmail.com", 
-  lis_user <- getOption("lis_user")             #         "gmail_password" = "my_password")
-  lis_password <- getOption("lis_password")     # also confirm that https://myaccount.google.com/lesssecureapps is on
-  
-  purrr::walk(ccs, function(cc) {
+get_lis_ginis <- function(ccs, wait = 240) {
+  yahoo_user <- getOption("yahoo_user")        # set all of these in .Rprofile, e.g.:
+  yahoo_password <-getOption("yahoo_password") # options("yahoo_user" = "juanita-herrera@yahoo.com", 
+  lis_user <- getOption("lis_user")            #         "yahoo_password" = "app_password")
+  lis_password <- getOption("lis_password")    # note you'll have to set up an app password for R in
+  purrr::walk(ccs, function(cc) {              # your Yahoo account and use *that*, not your account password
     lissy <- readLines("R/lissy.R") %>% 
       paste(collapse = "\n") %>% 
       str_replace("lis_user", lis_user) %>% 
       str_replace("lis_password", lis_password) %>% 
       str_replace_all("CCODE", cc)
-
-    mailR::send.mail(from = gmail_user,
-                     to = "postbox@lisdatacenter.org",
-                     subject = cc,
-                     body = lissy,
-                     smtp = list(host.name = "smtp.gmail.com",
-                                 port = 465, 
-                                 user.name = gmail_user, 
-                                 passwd = gmail_password,
-                                 ssl = TRUE),
-                     authenticate = TRUE,
-                     send = TRUE)
+    
+    send.mail(from = yahoo_user,
+              to = "postbox@lisdatacenter.org",
+              subject = cc,
+              body = lissy, 
+              authenticate = TRUE,
+              smtp = list(host.name = "smtp.mail.yahoo.com",
+                          port = 587,
+                          user.name = yahoo_user,
+                          passwd = yahoo_password,
+                          tls = TRUE))
     
     if (length(ccs) > 1) Sys.sleep(wait)
   })
